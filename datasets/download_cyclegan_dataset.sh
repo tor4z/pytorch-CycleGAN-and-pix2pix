21314bda1e7717917d6cbaef3a45aa4fb25c25f9
tor4z <vwenjie@hotmail.com>
2019-11-22 12:57:05 +0800

    Update download script

--- datasets/download_cyclegan_dataset.sh
+++ datasets/download_cyclegan_dataset.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 FILE=$1
 
 if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" &&  $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "mini" && $FILE != "mini_pix2pix" && $FILE != "mini_colorization" ]]; then
@@ -11,11 +13,13 @@ if [[ $FILE == "cityscapes" ]]; then
     exit 1
 fi
 
-echo "Specified [$FILE]"
-URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
-ZIP_FILE=./datasets/$FILE.zip
-TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $ZIP_FILE
+# echo "Specified [$FILE]"
+URL="https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/${FILE}.zip"
+ZIP_FILE="./datasets/${FILE}.zip"
+TARGET_DIR="./datasets/${FILE}/"
+
+wget $URL
+
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
